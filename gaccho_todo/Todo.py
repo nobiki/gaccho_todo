a415d61486614a58fe274145c3b514e80729a212
Naoaki Obiki <obiki.wk@gmail.com>
2017-11-01 10:10:30 +0900

    init.

---
+++ gaccho_todo/Todo.py
@@ -0,0 +1,17 @@
+from Article import Article
+
+from datetime import datetime
+from time import mktime
+
+class Todo(Article):
+    def color_pair(self):
+        return {"text":"BLACK", "back":"YELLOW"}
+
+    def get(self):
+
+        return [
+                ("ToDo", "2017-10-19 10:00:00", "投稿者名", "あれをやる", "http://example.com/", "てきすとてきすとてきすとてきすと"),
+                ("ToDo", "2017-10-20 10:00:01", "投稿者名", "これをやる", "http://example.com/", "てきすとてきすとてきすとてきすと"),
+                ("ToDo", "2017-10-21 10:00:02", "投稿者名", "あれもやる", "http://example.com/", "てきすとてきすとてきすとてきすと"),
+                ("ToDo", "2017-10-22 10:00:00", "投稿者名", "これもやる", "http://example.com/", "てきすとてきすとてきすとてきすと"),
+                ("ToDo", "2017-10-19 10:00:01", "投稿者名", "あれもこれもやる", "http://example.com/", "てきすとてきすとてきすとてきすと"),
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+        name             = 'gaccho_todo',
+        version          = '0.0.1',
+        description      = 'Gaccho ToDo Feed Plugin',
+        license          = 'MIT',
+        author           = 'nobiki',
+        author_email     = 'test@example.com',
+        url              = 'https://github.com/nobiki/gaccho_todo.git',
+        keywords         = 'gaccho todo',
+        packages         = find_packages(),
+        install_requires = [],
+        )
+
