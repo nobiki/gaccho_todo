afaa0bf91ac8f122919606ccf3f4fb708cb868f8
Naoaki Obiki <obiki.wk@gmail.com>
2017-11-01 10:29:00 +0900

    modified:   gaccho_todo/Todo.py

--- gaccho_todo/Todo.py
+++ gaccho_todo/Todo.py
@@ -2,14 +2,14 @@ from Article import Article
 
 class Todo(Article):
     def color_pair(self):
-        return {"text":"YELLOW", "back":"CYAN"}
+        return {"text":"CYAN", "back":"MAGENTA"}
 
     def get(self):
 
         return [
                 ("Todo", "2017-10-19 10:00:00", "投稿者名", "あれやる", "http://example.com/", "てきすとてきすとてきすとてきすと"),
                 ("Todo", "2017-10-20 10:00:01", "投稿者名", "これやる", "http://example.com/", "てきすとてきすとてきすとてきすと"),
-                ("Todo", "2017-10-21 10:00:02", "投稿者名", "あれもこれもやるはvalueとおなじ", "http://example.com/", "てきすとてきすとてきすとてきすと"),
+                ("Todo", "2017-10-21 10:00:02", "投稿者名", "あれもこれもやる", "http://example.com/", "てきすとてきすとてきすとてきすと"),
                 ("Todo", "2017-10-22 10:00:00", "投稿者名", "あれやって", "http://example.com/", "てきすとてきすとてきすとてきすと"),
                 ("Todo", "2017-10-19 10:00:01", "投稿者名", "これやって", "http://example.com/", "てきすとてきすとてきすとてきすと"),
                 ("Todo", "2017-10-27 10:00:02", "投稿者名", "あれとこれやって", "http://example.com/", "てきすとてきすとてきすとてきすと"),
